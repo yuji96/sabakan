6b31c9a56e64147fd310109961ecd054e4ed2808
yuji96 <y.yama683@gmail.com>
2023-04-05 17:58:50 +0900

    initial commit

---
+++ app.py
@@ -0,0 +1,66 @@
+from pathlib import Path
+from pprint import pprint  # noqa
+
+import pandas as pd
+import plotly.graph_objects as go
+import streamlit as st
+from PIL import Image
+
+from nvidia import parse_nvidia_smi
+
+# read data
+# TODO: アプリの中で起動時に読み込む
+xml = Path("sample/nvidia-smi.xml").read_text()
+data = parse_nvidia_smi(xml)
+
+# visualize
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns([1, 8])
+with col1:
+    st.image(Image.open("logo.png"), width=100)
+with col2:
+    st.title("サーバー管理モニター")
+
+# FIXME: これを逐次的に溜めていく？
+colors = {"free": "gray"}
+
+
+columns = st.columns(len(data))
+for (gpu_index, proc), col in zip(data.items(), columns):
+    with col:
+        st.header(f"GPU: {gpu_index}")
+
+        proc = pd.DataFrame(proc)
+        proc["used_memory_int"] = proc["used_memory"].str.replace(" MiB", "").astype(int)
+        proc.sort_values("used_memory_int", inplace=True, ascending=False)
+
+        # FIXME: ハードコーディング
+        free = {
+            "user": "free",
+            "used_memory_int": 24000 - proc["used_memory_int"].sum(),
+        }
+        free["used_memory"] = str(free["used_memory_int"]) + " MiB"
+        proc = pd.concat([proc, pd.DataFrame([free])], axis="rows", ignore_index=True)
+
+        fig = go.Figure(
+            data=[
+                go.Pie(
+                    labels=proc["user"],
+                    values=proc["used_memory_int"],
+                    sort=False,
+                    direction="clockwise",
+                    marker=dict(colors=[colors.get(label) for label in proc["user"]]),
+                )
+            ],
+            layout=dict(legend=dict(orientation="h", xanchor="center", x=0.5)),
+        )
+        st.plotly_chart(fig, use_container_width=True)
+
+        st.dataframe(
+            proc.iloc[:][
+                ["user", "used_memory", "cpu_usage", "cum_time", "pid", "process_name"]
+            ].style.background_gradient(
+                gmap=proc["used_memory_int"], axis="rows", vmin=0, vmax=24000, low=0.5
+            )
+        )
---
+++ nvidia.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from pprint import pprint
+
+import xmltodict
+from paramiko import SSHClient
+
+
+def proc_info(pid, ssh: SSHClient):
+    # https://linuxjm.osdn.jp/html/procps/man1/ps.1.html
+    command = f"ps -ww -p {pid} -o user:20,cp,time,etime,cmd"
+    # TODO: srderr について異常系
+    stdin, stdout, stderr = ssh.exec_command(command, timeout=1)
+    stdin.close()
+
+    header, info = stdout.read().decode("utf8").splitlines()
+    return dict(
+        zip(["user", "cpu_usage", "cpu_time", "cum_time", "cmd"], info.split(maxsplit=4))
+    )
+
+
+def parse_nvidia_smi(xml, ssh):
+    d = xmltodict.parse(xml)
+    gpu_info: dict[int, list] = {}
+    for gpu in d["nvidia_smi_log"]["gpu"]:
+        gpu_index = int(gpu["minor_number"])
+        gpu_info[gpu_index] = []
+
+        # FIXME: 空き GPU だと processes の返り値が None らしい
+        processes = gpu["processes"]["process_info"]
+        if not isinstance(processes, list):
+            processes: list[dict] = [processes]
+        for proc in processes:
+            # TODO: プロセスごとの GPU 使用率も取得したい。
+            proc.update(proc_info(proc["pid"], ssh))
+            gpu_info[gpu_index].append(proc)
+    return gpu_info
+
+
+if __name__ == "__main__":
+    xml = Path("sample/nvidia-smi.xml").read_text()
+    res = parse_nvidia_smi(xml)
+    pprint(res)
