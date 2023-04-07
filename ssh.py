4642ac727499c0c6292cf6f50b756fcdb44d13dc
yuji96 <y.yama683@gmail.com>
2023-04-07 11:15:40 +0900

    複数サーバとの ssh 接続を並列化

--- ssh.py
+++ ssh.py
@@ -1,6 +1,36 @@
 import traceback
+from multiprocessing import Pool
 
 import paramiko
+
+
+def worker(args):
+    secret, command, cmd_args, name, timeout_client, timeout_cmd, replace_cmd = args
+    with paramiko.SSHClient() as client:
+        client.load_host_keys(secret["ssh"]["known_hosts_path"])
+        private_key = paramiko.RSAKey.from_private_key_file(
+            secret["ssh"]["secret_key_path"], secret["ssh"]["passphrase"]
+        )
+
+        try:
+            client.connect(
+                secret["servers"][name]["host"],
+                username=secret["user"],
+                pkey=private_key,
+                timeout=timeout_client,
+            )
+            if replace_cmd:
+                replaced = secret["servers"][name][command]
+            stdin, stdout, stderr = client.exec_command(
+                f"{replaced} {cmd_args}", timeout=timeout_cmd
+            )
+            return {
+                "stdout": stdout.read().decode("utf8"),
+                "stderr": stderr.read().decode("utf8"),
+            }
+        except Exception:
+            # TODO
+            print(traceback.format_exc())
 
 
 def ssh(
@@ -12,42 +42,22 @@ def ssh(
     timeout_client=10,
     replace_cmd=False,
 ):
-    rsa_key = paramiko.RSAKey.from_private_key_file(
-        secret["ssh"]["secret_key_path"], secret["ssh"]["passphrase"]
-    )
     if names is None:
         names = secret["servers"].keys()
+    else:
+        # TODO: 要素数変わったら警告
+        names = [name for name in names if name in secret["servers"]]
 
-    with paramiko.SSHClient() as client:
-        try:
-            client.load_host_keys(secret["ssh"]["known_hosts_path"])
-
-            res = {}
-            for name in names:
-                info = secret["servers"].get(name)
-                if info is None:
-                    # TODO: warning?
-                    continue
-                client.connect(
-                    info["host"],
-                    username=secret["user"],
-                    pkey=rsa_key,
-                    timeout=timeout_client,
-                )
-
-                if replace_cmd:
-                    replaced = info[command]
-                stdin, stdout, stderr = client.exec_command(
-                    f"{replaced} {args}", timeout=timeout_cmd
-                )
-                res[name] = {
-                    "stdout": stdout.read().decode("utf8"),
-                    "stderr": stderr.read().decode("utf8"),
-                }
-            return res
-        except Exception:
-            # TODO
-            print(traceback.format_exc())
+    # TODO: ssh を並列実行するライブラリに移行
+    with Pool(processes=len(names)) as pool:
+        results = pool.map(
+            worker,
+            [
+                (secret, command, args, name, timeout_client, timeout_cmd, replace_cmd)
+                for name in names
+            ],
+        )
+    return dict(zip(names, results))
 
 
 if __name__ == "__main__":
